ea97e9ff598f96428dbbab9f1197ae34a22dc7dc
Juan Langlois <jlangloi@stanford.edu>
2021-06-26 13:02:32 -0700

    fixed simple bugs in student_data_api

--- src/d01_data/student_data_api.py
+++ src/d01_data/student_data_api.py
@@ -1,4 +1,7 @@
 import pandas as pd
+import sys
+sys.path.append('../..')
+
 
 from src.d01_data.abstract_data_api import AbstractDataApi
 
@@ -18,8 +21,19 @@ class StudentDataApi(AbstractDataApi):
         if periods_list is None:
             periods_list = ["1920"]
 
-        df = pd.DataFrame()
+        df = None
         for period in periods_list:
-            df = df.append(self.read_data(_student_file_path.format(period=period)))
+            df_period = self.read_data(_student_file_path.format(period=period))
+            df_period.drop(columns=['Unnamed: 0'], inplace=True)
+            if df is None:
+                df = df_period
+            else:
+                df = df.append(df_period, ignore_index=True)
 
         return df
+
+if __name__ == "__main__":
+    # execute only if run as a script
+    student_data_api = StudentDataApi()
+    df = student_data_api.get_data(periods_list=["1819", "1920"])
+    print(df)
