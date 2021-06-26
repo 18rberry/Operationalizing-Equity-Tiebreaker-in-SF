4d6910732a72ba734c9a0f5ba8d51fb1f9dd95b9
jilanglois <56458805+jilanglois@users.noreply.github.com>
2021-06-26 12:50:08 -0700

    eliminate unused code in data api

--- src/d01_data/abstract_data_api.py
+++ src/d01_data/abstract_data_api.py
@@ -4,41 +4,40 @@ import numpy as np
 _raw_file_path = "/share/data/school_choice/"
 _out_file_path = _raw_file_path + "dssg/"
 
+
 class AbstractDataApi:    
     def __init__(self):
         pass
-    
-    def read_dta(self, path, ratio, nrows):
+
+    @staticmethod
+    def read_dta(path):
         return pd.read_stata(path)
-    
-    def read_csv(self, path, ratio, nrows):
-        if nrows is not None:
-            return pd.read_csv(path, nrows=nrows, low_memory=False)
-        if ratio is not None:
-            return pd.read_csv(path, skiprows=lambda x: np.random.rand() > ratio, low_memory=False)
-        
+
+    @staticmethod
+    def read_csv(path):
         df = pd.read_csv(path, low_memory=False)
         
         return df
-    
-    def read_excel(self, path, ratio, nrows):
+
+    @staticmethod
+    def read_excel(path):
         df = pd.read_excel(path, sheet_name=None)
         
         return df
-        
 
-    def read_data(self, file_name, ratio=None, nrows=None):
+    def read_data(self, file_name):
         file_type = file_name.split(".")[-1]
         path = _raw_file_path + file_name
             
         if file_type == "csv":
-            return self.read_csv(path, ratio, nrows)
+            return self.read_csv(path)
         elif file_type == "dta":
-            return self.read_dta(path, ratio, nrows)
+            return self.read_dta(path)
         elif file_type == "xlsx":
-            return self.read_excel(path, ratio, nrows)
+            return self.read_excel(path)
         else:
             raise Exception("Format .%s not implemented" % file_type)
 
-    def save_data(self, df, file_name):
+    @staticmethod
+    def save_data(df, file_name):
         df.to_csv(_out_file_path + file_name)
