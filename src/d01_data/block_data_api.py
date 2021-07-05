94577c51272ff469afc393a09eb8e35d1e583111
Gabriel Agostini <agostini@soal-2.stanford.edu>
2021-07-05 12:57:29 -0700

    added classification method and column renaming

--- src/d01_data/block_data_api.py
+++ src/d01_data/block_data_api.py
@@ -53,6 +53,13 @@ class BlockDataApi(AbstractDataApi):
                 self._cache_demographic['fields'] = df_dict['field descriptions']
                 self._cache_demographic['data'] = df_dict['block database']
                 
+                #Rename columns so that block dataframe and field dataframe match:
+                self._cache_demographic['data'] = self._cache_demographic['data'].rename(columns={"AREA": "Area"})
+                
+                old = ['avg CSTELA score 2006-14', 'CSTELA test takers 2006-14']
+                new = ['avg CSTELA score 2006-10', 'CSTELA test takers 2006-10']
+                self._cache_demographic['fields']['Field Name'] = self._cache_demographic['fields']['Field Name'].replace(old, new)
+                
                 # Clean the Fields dataframe from NaN columns and rows:
                 self._cache_demographic['fields'] = self._cache_demographic['fields'].dropna(axis=0, how='all')
                 self._cache_demographic['fields'] = self._cache_demographic['fields'].dropna(axis=1, how='all')
@@ -88,6 +95,60 @@ class BlockDataApi(AbstractDataApi):
             df = self._cache_demographic['fields'].copy()
         df.set_index('Field Name', inplace=True)
         return df.loc[columns]
+    
+    #classify the columns of the block dataframe according to themes
+    def get_classification(self, classification="first_round"):
+        
+        #parameter referring to classification will allow us to experiment with other classifications. So far only one.
+        if classification == "first_round":
+            
+            from collections import defaultdict
+
+            field_list = list(block_df.columns)
+
+            race_words = ["Black", "African", "Filipino", "Asian", "Hisp", "White", "Samoa", "Mixed", "Pacific",
+                          "Decl", "Other", "DS", "Wht", "AALPI", "Asn", "AfAm"]
+            income_words = ["income", "Lunch", "poverty"]
+            housing_words = ["Hsng", "SFHA", "Hsg"]
+            testing_words = ["SBAC", "CST", "test"]
+
+            group_dict = defaultdict(list)
+
+            for col in field_list:
+                idx = field_list.index(col)
+                #The first columns are identification columns:
+                if idx <= field_list.index("SF Analysis Neighborhood"):
+                    group_dict["ID"].append(col)
+                #Then there are CTIP-related columns:
+                elif idx <= field_list.index("Possible change in CTIP1 status Model 2"):
+                    group_dict["CURRENT"].append(col)
+                #Now let's take all the ethnicity-related columns, starting with the most detailed breakdown:
+                elif "F201" in col:
+                    group_dict["ETHNICITY_DETAILED"].append(col)
+                #The second level of detailed ethnicity (grouped for a few grades):
+                elif col[6] == "-" and "stu" not in col:
+                    group_dict["ETHNICITY_DETAILED_GROUP"].append(col)
+                #Non-detailed ethnicity:
+                elif any(word in col for word in race_words):
+                    group_dict["ETHNICITY"].append(col)
+                #Income-related columns:
+                elif any(word in col for word in income_words):
+                    group_dict["INCOME"].append(col)
+                #Housing-related columns:
+                elif any(word in col for word in housing_words):
+                    group_dict["HOUSING"].append(col)
+                #Testing-related columns:
+                elif any(word in col for word in testing_words):
+                    group_dict["TEST"].append(col)
+                #All others are general demographic info:
+                else:
+                    group_dict["DEMOGRAPHIC"].append(col)
+            
+            return group_dict
+
+        else:
+            print("This classification has not been defined")
+            return None
 
 
 if __name__ == "__main__":
