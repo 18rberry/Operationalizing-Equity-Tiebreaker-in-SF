775321130da9e37b822bcecea25f09c2c9416a28
Juan Langlois <jlangloi@stanford.edu>
2021-07-19 07:35:21 -0700

    add naive classifier using rate

--- src/d04_modeling/naive_classifier.py
+++ src/d04_modeling/naive_classifier.py
@@ -3,16 +3,22 @@ from src.d04_modeling.knapsack_approx import KnapsackApprox
     
 class NaiveClassifier(AbstractBlockClassifier):
     results = None
-    def __init__(self, true_group='nFRL', false_group='nOther'):
+    def __init__(self, true_group='nFRL', false_group='nOther', rate=False):
+        self.rate = rate
         super().__init__(true_group, false_group)
     
     def get_roc(self, fpr=None):
         if self.results is None:
-            results = self.data.copy().sort_values(self.true_group, ascending=False)
+            results = self.data.copy()
+            if self.rate:
+                fun = lambda row: row[self.true_group] / float(row['n'])
+                results['rate'] = results.apply(fun, axis=1, raw=False)
+                results.sort_values(['rate', self.true_group], ascending=False, inplace=True)
+            else:
+                results.sort_values(self.true_group, ascending=False, inplace=True)
 
-            results.rename(columns={self.true_group: 'tp', self.false_group: 'fp' }, inplace=True)
-            results['tp'] = results['tp'].cumsum()
-            results['fp'] = results['fp'].cumsum()
+            results['tp'] = results[self.true_group].cumsum()
+            results['fp'] = results[self.false_group].cumsum()
 
             results['tpr'] = results['tp'] / self.data[self.true_group].sum()
             results['fpr'] = results['fp'] / self.data[self.false_group].sum()
