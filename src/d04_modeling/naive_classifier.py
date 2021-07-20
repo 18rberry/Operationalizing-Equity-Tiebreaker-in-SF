6975d71a0fca418dc22624a19bb77e7bdde21100
Gabriel Agostini <agostini@soal-2.stanford.edu>
2021-07-20 16:20:53 -0700

    fixed syntax

--- src/d04_modeling/naive_classifier.py
+++ src/d04_modeling/naive_classifier.py
@@ -2,9 +2,16 @@ from src.d04_modeling.abstract_block_classifier import AbstractBlockClassifier
 
 
 class NaiveClassifier(AbstractBlockClassifier):
-    def __init__(self, positive_group='nFRL', negative_group='nOther', rate=False):
+    def __init__(self, positive_group='nFRL', negative_group='nOther', rate=False, user=""):
+        
+        self.positive_group = positive_group
+        self.negative_group = negative_group
+        columns = [self.positive_group]
+    
+        super().__init__(columns, positive_group=self.positive_group, negative_group=self.negative_group, user=user)
+        
+        #Solving the naive classification problem:
         self.rate = rate
-        super().__init__(positive_group, negative_group)
         self.results = self.get_results()
         
     def get_results(self):
@@ -23,7 +30,7 @@ class NaiveClassifier(AbstractBlockClassifier):
         results['fpr'] = results['fp'] / self.data[self.negative_group].sum()
         return results
     
-    def get_roc(self, fpr=None):        
+    def get_roc(self, param_arr=None):        
         return self.results[['fpr', 'tpr']].copy()
     
     def get_solution_set(self, fpr):
