3be96d5e0c5a8681dc38e668a9244ece0dd163e1
Gabriel Agostini <agostini@soal-2.stanford.edu>
2021-08-03 13:57:31 -0700

    added function to measure intersection tpr

--- src/d04_modeling/abstract_block_classifier.py
+++ src/d04_modeling/abstract_block_classifier.py
@@ -223,7 +223,7 @@ class AbstractBlockClassifier:
         ax = self.__classifier_data_api.plot_map_column(map_df_data=map_df_data, col=col, ax=ax,
                                                         legend=False, title=title, save=save)
         return ax
-        
+    
     def get_confusion_matrix(self, params):
         """
         returns pandas.DataFrame with confusion matrix for a given parameter list
@@ -232,7 +232,7 @@ class AbstractBlockClassifier:
             params_key = tuple(params)
         else:
             params_key = params
-            
+
         if params_key in self.confusion_dict.keys():
             confusion_matrix = self.confusion_dict[params_key]
         else:
@@ -242,15 +242,15 @@ class AbstractBlockClassifier:
             col = 'Real\Pred'
             data[col] = 0
             data.loc[solution_set, col] = 1
-            
+
             confusion_matrix = data.groupby(col).sum()
             confusion_matrix = confusion_matrix.transpose()
-            
+
             if 0 not in confusion_matrix.columns:
                 confusion_matrix[0] = 0
             if 1 not in confusion_matrix.columns:
                 confusion_matrix[1] = 0
-            
+
             confusion_matrix = confusion_matrix[[1, 0]]
             self.confusion_dict[params_key] = confusion_matrix
             
@@ -277,3 +277,24 @@ class AbstractBlockClassifier:
     def precision(self, params):
         confusion_matrix_arr = self.get_confusion_matrix(params).values
         return confusion_matrix_arr[0,0]/(confusion_matrix_arr[0,0] + confusion_matrix_arr[1,0])
+    
+    def intersection_tpr(self, params):
+        
+        if self.positive_group == "nBoth":
+            return self.tpr(params)
+        
+        else:
+            
+            data = self.full_data.copy()[["nBoth"]]
+
+            solution_set = self.get_solution_set(params)
+            col = 'tiebreaker'
+            data[col] = 0
+            data.loc[solution_set, col] = 1
+
+            grouped_data = data.groupby(col).sum()
+            grouped_data_arr = grouped_data.values
+            
+            inter_tpr = grouped_data_arr[1]/(grouped_data_arr[0] + grouped_data_arr[1])
+            
+        return inter_tpr[0]
